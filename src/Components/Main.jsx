382c0486de3c3f025df0f215f160e108bcc3d719
Nikhil Bokade <nikhilbokade25@gmail.com>
2024-04-14 18:42:25 -0400

    Fetching data from API and displaying names

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css"
+import axios from "axios";
 
 export default function Main(){
+
+    const [pokemonData, setPokemonData] = useState([]);
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=500";
+
+    useEffect (() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+                const pokemonList = response.data.results;
+                const pokemonDetails = await Promise.all(
+                    pokemonList.map(async (pokemon) => {
+                        const result = await axios.get(pokemon.url);
+                        return result.data;
+                    })
+                );
+                setPokemonData(pokemonDetails);
+                console.log(pokemonDetails); 
+            } catch (error) {
+                console.error("Error fetching Pokémon data:", error);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+   
+
     return(
         <div className="main_container"> 
             <div className="navbar">
@@ -15,6 +42,14 @@ export default function Main(){
                     // value={searchTerm}
                     // onChange={handleSearch}
                 />
+            </div>
+
+            <div className="pokemon_list">
+                {pokemonData.map((pokemon, index)=>(
+                    <div key={index} className="pokemon">
+                        <p>{pokemon.name}</p>    
+                    </div>
+                ))}
             </div>
         </div>
     );
